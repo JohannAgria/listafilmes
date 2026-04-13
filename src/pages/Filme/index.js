16f088ee0a5388be93d6a302fff48c17a27dc82b
JohannAgria <johann.agria@outlook.com>
2026-04-13 12:39:31 -0300

    quarto teste

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate, } from "react-router-dom"
 import api from "../../services/api";
-import './filme.css'
+import './Filme.css'
 
 export default function Filme() {
     const { id } = useParams();
