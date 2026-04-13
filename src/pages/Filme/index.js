ff6201293317f7cca78b25576fc5360cee8afcfd
JohannAgria <johann.agria@outlook.com>
2026-04-13 12:37:04 -0300

    terceiro teste

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, replace } from "react-router-dom"
+import { useParams, useNavigate, } from "react-router-dom"
 import api from "../../services/api";
 import './filme.css'
 
